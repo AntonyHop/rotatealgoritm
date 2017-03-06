41088a3adf06700d780761bfa58549626b647f8c
Antony <stargeit123@gmail.com>
2017-03-06 12:02:01 +0200

    update ckypter

--- rotateGrid/crypter_decrypter.h
+++ rotateGrid/crypter_decrypter.h
@@ -34,17 +34,17 @@ string CryptWord(string original, int gridSize, int **first, int **second, int *
 		four_part += original[i];
 	}
 	cout << four_part << endl;
-	ShowGrid(first,gridSize);
+	//ShowGrid(first,gridSize);
 	int counter = 0;
 	for (int i = 0; i < gridSize; i++) {
 		for (int j = 0; j < gridSize; j++) {
 			if(first[i][j] == 0 && counter<  first_part.size()){
 				result += first_part [counter];
-				cout << counter << " " << first_part[counter] <<" " << first_part.size()<< endl;
+				//cout << counter << " " << first_part[counter] <<" " << first_part.size()<< endl;
 				counter++;
 			}
 			else {
-				result += '0';
+				result += char(10+rand()%30);
 			}
 		}
 	}
@@ -54,11 +54,11 @@ string CryptWord(string original, int gridSize, int **first, int **second, int *
 		for (int j = 0; j < gridSize; j++) {
 			if (second[i][j] == 0 && counter<  second_part.size()) {
 				result += second_part[counter];
-				cout << counter << " " << second_part[counter] << " " << second_part.size() << endl;
+				//cout << counter << " " << second_part[counter] << " " << second_part.size() << endl;
 				counter++;
 			}
 			else {
-				result += '1';
+				result += char(10+rand()%30);
 			}
 		}
 	}
@@ -67,11 +67,11 @@ string CryptWord(string original, int gridSize, int **first, int **second, int *
 		for (int j = 0; j < gridSize; j++) {
 			if (third[i][j] == 0 && counter<  third_part.size()) {
 				result += third_part[counter];
-				cout << counter << " " << third_part[counter] << " " << third_part.size() << endl;
+				//cout << counter << " " << third_part[counter] << " " << third_part.size() << endl;
 				counter++;
 			}
 			else {
-				result += '2';
+				result += char(10+rand()%30);
 			}
 		}
 	}
@@ -80,13 +80,79 @@ string CryptWord(string original, int gridSize, int **first, int **second, int *
 		for (int j = 0; j < gridSize; j++) {
 			if (four[i][j] == 0 && counter<  four_part.size()) {
 				result += four_part[counter];
-				cout << counter << " " << four_part[counter] << " " << four_part.size() << endl;
+				//cout << counter << " " << four_part[counter] << " " << four_part.size() << endl;
 				counter++;
 			}
 			else {
-				result += '3';
+				result += char(10+rand()%30);
 			}
 		}
 	}
+	return result;
+}
+
+
+string DeCryptWord(string original, int gridSize, int **first, int **second, int **third, int **four) {
+	string result;
+	string first_part;
+	string second_part;
+	string third_part;
+	string four_part;
+
+	for (int i = 0; i < 1*(original.size()/4); i++) {
+		first_part+= original[i];
+	}
+	int counter = 0;
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			if(first[i][j] == 0 && counter<  first_part.size()){
+				result += first_part [counter];
+				//cout << counter << " " << first_part[counter] <<" " << first_part.size()<< endl;
+			}
+			counter++;
+		}
+	}
+	for (int i = 1*(original.size()/4); i < 2*(original.size()/4); i++) {
+		second_part+= original[i];
+	}
+	counter = 0;
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			if(second[i][j] == 0 && counter<  second_part.size()){
+				result += second_part [counter];
+				//cout << counter << " " << second_part[counter] <<" " << second_part.size()<< endl;
+			}
+			counter++;
+		}
+	}
+
+	for (int i = 2*(original.size()/4); i < 3*(original.size()/4); i++) {
+		third_part+= original[i];
+	}
+	counter = 0;
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			if(third[i][j] == 0 && counter<  third_part.size()){
+				result += third_part [counter];
+				//cout << counter << " " << third_part[counter] <<" " << third_part.size()<< endl;
+			}
+			counter++;
+		}
+	}
+
+	for (int i = 3*(original.size()/4); i < 4*(original.size()/4); i++) {
+		four_part+= original[i];
+	}
+	counter = 0;
+	for (int i = 0; i < gridSize; i++) {
+		for (int j = 0; j < gridSize; j++) {
+			if(four[i][j] == 0 && counter<  four_part.size()){
+				result += four_part [counter];
+				//cout << counter << " " << four_part[counter] <<" " << four_part.size()<< endl;
+			}
+			counter++;
+		}
+	}
+	cout << endl;
 	return result;
 }
